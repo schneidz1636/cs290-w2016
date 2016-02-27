1638c16b2930a6c3086ee0b388252fb78742ddbe
cavemanAGz <woodal@oregonstate.edu>
2016-02-27 15:08:10 -0800

    better style

--- master/data_isbn_into_db.php
+++ master/data_isbn_into_db.php
@@ -10,30 +10,47 @@ $authors = htmlspecialchars($_SESSION['authorsTemp']);
 $publisher = htmlspecialchars($_SESSION['publisherTemp']);
 $pageCount = htmlspecialchars($_SESSION['pageCountTemp']);
 ?>
-​
-​
+
+
 <div class="container">
 	<h1>Add Book</h1>
 		<div class="row">
 			<div class="col-lg-6">
 				<div class="well" background color="f8f8f8">
 					<form method="post" action='book_receive.php' class="inform">
-						<ul class="list-group">
-​
-							<li class="list-group-item"><label>ISBN:</label> <input type="text" name ="isbn" value=<?php echo $identifier;?> disabled>
-							<li class="list-group-item"><label>Title:</label> <input type="text" name ="name" value="<?php echo $title;?>">
-							<li class="list-group-item"><label>Author:</label> <input type="text" name ="author" value="<?php echo $authors;?>">
-							<li class="list-group-item"><label>Publisher:</label> <input type="text" name ="publisher" value="<?php echo $publisher;?>">
-							<li class="list-group-item"><label>Page count:</label> <input type="text" name ="pageCount" value="<?php echo $pageCount;?>">
-							<li class="list-group-item"><label>Location:</label> <input type="text" name ="location">
-							<li class="list-group-item"><input type=submit>
-							
-						</ul>
+
+						<div class="form-group">
+							<label for="isbn">ISBN:
+							<input type="text" name ="isbn" value=<?php echo $identifier;?> disabled>
+						</div>
+						<div class="form-group">
+							<label>Title:
+							<input type="text" name ="name" value=<?php echo $title;?>>
+						</div>
+						<div class="form-group">
+							<label>Author:
+							<input type="text" name ="author" value=<?php echo $authors;?>>
+						</div>
+						<div class="form-group">
+							<label>Publisher:
+							<input type="text" name ="publisher" value=<?php echo $publisher;?>>
+						</div>
+						<div class="form-group">
+							<label>Page count:
+							<input type="text" name ="pageCount" value=<?php echo $pageCount;?>>
+						</div>
+						<div class="form-group">
+							<label>Location:
+							<input type="text" name ="location">
+						</div>
+							<input type=submit>
+							<button type="submit" class="btn btn-default" name="submit">Add Book</button>
+
 					</form>
 				</div>
 			</div>
 		</div>
 </div>
-​
+
 <?php } ?>
 <?php include("_footer.php");?>
