c72f03814a1283c6e791b4804900fbe8672f3062
Michael Humphrey <mhumphree@gmail.com>
2016-03-08 18:38:36 -0800

    Added infinite scrolling

---
+++ master/load_more_books.php
@@ -0,0 +1,24 @@
+<?php
+$mysqli = new mysqli("oniddb.cws.oregonstate.edu","grantch-db","rVrkAizgwSrdVJlU","grantch-db");
+
+if (!isset($_REQUEST["offset"])) {
+	http_response_code(400);
+} else {
+	$query = "select isbn,title,authors,publisher,pagecount from books LIMIT 20 OFFSET ". htmlspecialchars($_REQUEST["offset"]);
+	if($result = $mysqli->query($query)){
+		while($obj = $result->fetch_object()){
+			echo "<tr>";
+			echo "<td>".htmlspecialchars($obj->isbn)."</td>";
+			echo "<td>".htmlspecialchars($obj->title)."</td>";
+			echo "<td>".htmlspecialchars($obj->authors)."</td>";
+			echo "<td>".htmlspecialchars($obj->publisher)."</td>";
+			echo "<td>".htmlspecialchars($obj->pagecount)."</td>";
+			echo "</tr>";
+		}
+		
+		$result->close();
+	} else {
+		echo "<tr><td>Something went wrong. I'm sorry.</td></tr>";
+	}
+}
+?>
--- master/view_all_books.php
+++ master/view_all_books.php
@@ -8,8 +8,8 @@
 
 <?php
 $time_start = microtime(true);
-echo "<table class='table table-striped'><tr><th>ISBN<th>Title<th>Author<th>Publisher<th>Page count</tr>";
-if($result = $mysqli->query("select isbn,title,authors,publisher,pagecount from books")){
+echo "<table class='table table-striped' id='posts_table'><tr><th>ISBN<th>Title<th>Author<th>Publisher<th>Page count</tr>";
+if($result = $mysqli->query("select isbn,title,authors,publisher,pagecount from books limit 20")){
 	while($obj = $result->fetch_object()){
 		echo "<tr>";
 		echo "<td>".htmlspecialchars($obj->isbn)."</td>";
@@ -17,16 +17,45 @@ if($result = $mysqli->query("select isbn,title,authors,publisher,pagecount from 
 		echo "<td>".htmlspecialchars($obj->authors)."</td>";
 		echo "<td>".htmlspecialchars($obj->publisher)."</td>";
 		echo "<td>".htmlspecialchars($obj->pagecount)."</td>";
-		}
-		
-		$result->close();
+	}
+	
+	$result->close();
 }
 echo "</table>";
 $time_end = microtime(true);
 $extime = ($time_end - $time_start);
+echo "<div id='no-more-books' style='display: none;'><h3>No more books.</h3></div>";
 echo '<b>Time: </b>'.$extime.'<b> sec</b>';
+echo "<span style='display: none;' id='offset'>20</span>";
 
 ?>
+
+<script type="text/javascript">
+$(window).scroll(function() {
+    if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+		console.log("BOTTOM REACHED");
+		console.log("offset: " + $("#offset").text())
+		
+        $.ajax({
+        url: "load_more_books.php",
+		data: {offset: $("#offset").text()},
+        success: function(html, textStatus, jqXHR) {
+            if (html) {
+				$("#offset").text(+$("#offset").text() + 20);
+                $("#posts_table").append(html);
+            } else {
+				console.log("status: " + textStatus);
+				console.log("html: " + html);
+				$("#no-more-books").show();				
+            }
+        },
+		error: function(jqXHR, textStatus, errorThrown ) {
+			console.error(textStatus + ": " + errorThrown);
+		}
+        });
+    }
+});
+</script>
 
 <?php } ?>
 <?php include '_footer.php' ?>
