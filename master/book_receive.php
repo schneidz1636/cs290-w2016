bc0916acbae9c06b9f3bea5ad5168668ea7560da
Zachary Schneider <schneidz@flip1.engr.oregonstate.edu>
2016-03-10 11:33:58 -0800

    Added PHP regex to check bad_isbn is valid still

--- master/book_receive.php
+++ master/book_receive.php
@@ -8,45 +8,53 @@
 $book_in_main = "";
 $query = $mysqli->prepare("select isbn from books where isbn =?");
 $query->bind_param("i",$isbn);
-if($query->execute()){
-	if($query->fetch()){
-		$stmt = $mysqli->prepare("insert into user_has_book(user_iduser,book_isbn13,location) values(?,?,?)");
-		$isbn = htmlspecialchars($_REQUEST["isbn1"]);
-		$location = htmlspecialchars($_REQUEST["location"]);
-		$uid = htmlspecialchars($_SESSION["uid"]);
-		$stmt->bind_param("iis", $uid, $isbn, $location);
-		$stmt->execute();
-		$stmt->close();
-		$query->close();
-		$book_in_main = "FALSE";
-		
+
+if(preg_match('/^(\d{10}(\d{3})?)?$/', htmlspecialchars($_REQUEST["isbn1"]))){
+	
+
+
+	if($query->execute()){
+		if($query->fetch()){
+			$stmt = $mysqli->prepare("insert into user_has_book(user_iduser,book_isbn13,location) values(?,?,?)");
+			$isbn = htmlspecialchars($_REQUEST["isbn1"]);
+			$location = htmlspecialchars($_REQUEST["location"]);
+			$uid = htmlspecialchars($_SESSION["uid"]);
+			$stmt->bind_param("iis", $uid, $isbn, $location);
+			$stmt->execute();
+			$stmt->close();
+			$query->close();
+			$book_in_main = "FALSE";
+			
+		}
 	}
-}
 
-if($book_in_main == ""){
-		$stmt = $mysqli->prepare("insert into books(isbn,title,authors,publisher,pagecount) values(?,?,?,?,?)");
-		$isbn = htmlspecialchars($_REQUEST["isbn1"]);
-		$title = htmlspecialchars($_REQUEST["title"]);
-		$authors = htmlspecialchars($_REQUEST["authors"]);
-		$publisher = htmlspecialchars($_REQUEST["publisher"]);
-		$pageCount = htmlspecialchars($_REQUEST["pageCount"]);
-		$stmt->bind_param("isssi", $isbn, $title, $authors, $publisher, $pageCount);
-		$stmt->execute();
-		$stmt->close();
-	
+	if($book_in_main == ""){
+			$stmt = $mysqli->prepare("insert into books(isbn,title,authors,publisher,pagecount) values(?,?,?,?,?)");
+			$isbn = htmlspecialchars($_REQUEST["isbn1"]);
+			$title = htmlspecialchars($_REQUEST["title"]);
+			$authors = htmlspecialchars($_REQUEST["authors"]);
+			$publisher = htmlspecialchars($_REQUEST["publisher"]);
+			$pageCount = htmlspecialchars($_REQUEST["pageCount"]);
+			$stmt->bind_param("isssi", $isbn, $title, $authors, $publisher, $pageCount);
+			$stmt->execute();
+			$stmt->close();
+		
 
-	
-		$stmt = $mysqli->prepare("insert into user_has_book(user_iduser,book_isbn13,location) values(?,?,?)");
-		$uid = htmlspecialchars($_SESSION["uid"]);
-		$location = htmlspecialchars($_REQUEST["location"]);
-		$stmt->bind_param("iis", $uid, $isbn, $location);
-		$stmt->execute();
-		$stmt->close();
-	
+		
+			$stmt = $mysqli->prepare("insert into user_has_book(user_iduser,book_isbn13,location) values(?,?,?)");
+			$uid = htmlspecialchars($_SESSION["uid"]);
+			$location = htmlspecialchars($_REQUEST["location"]);
+			$stmt->bind_param("iis", $uid, $isbn, $location);
+			$stmt->execute();
+			$stmt->close();
+		
+	}else{
+		printf("Error: %s\n", $mysqli->error);
+	}	
 }else{
-	printf("Error: %s\n", $mysqli->error);
-}	
-
+	$message = "Please enter a ISBN of 10 or 13 digits."
+	echo "<script type='text/javascript'>alert('$message');</script>";
+}
 ?>
 
 
